8f5217f5dfcb0b1380905cf73030d6f5453b033d
VladWolf <vladwolf322@gmail.com>
2022-11-25 15:57:33 +0200

    for_test

--- shop.py
+++ shop.py
@@ -2,26 +2,29 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-
+    
     def get_total(self, amount):
-        return self.price * amount
-
+        return round(self.price * amount, 2) # round((self.price * 100)*amount /100, 2)
+    
 class ShoppingCart:
     def __init__(self):
-        self.lst = []
+        self.purchases = []
+        self.goods_price = []
 
     def add(self, product, amount):
-        for i in range(amount):
-            self.lst.append(product)
-
+        goods = [product for i in range(amount)]
+        self.goods_price.append(product.price*amount)
+        self.purchases.append(goods)
+    
     def get_total(self):
-        return sum([i.price for i in self.lst])
+        return round(sum(self.goods_price), 2)
 
-product = Product("Beer", 32.0)
-print(product.get_total(10))
+product = Product("Beer", 32.23)
+print(product.get_total(12))
 
 cart = ShoppingCart()
-cart.add(product, 2)
+cart.add(product, 4)
+print(cart.purchases)
 print(cart.get_total())
 
 # Волковський Владислав
